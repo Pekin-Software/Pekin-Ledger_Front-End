02b6379b9d531549076a1da476131d5abdd137e8
eskainah <eskainah@gmail.com>
2025-06-11 23:10:46 +0000

    swap url

--- src/components/Authentications/authentication.jsx
+++ src/components/Authentications/authentication.jsx
@@ -42,8 +42,8 @@ function SignInForm({ navigate }) {
   const handleLogin = async (e) => {
     e.preventDefault(); 
     try {
-      const response = await fetch("http://localhost:8000/api/auth/login/", {
-      // const response = await fetch("https://pekingledger.store/api/auth/login/", {
+      // const response = await fetch("http://localhost:8000/api/auth/login/", {
+      const response = await fetch("https://pekingledger.store/api/auth/login/", {
       
         method: "POST",
         headers: { "Content-Type": "application/json" },
