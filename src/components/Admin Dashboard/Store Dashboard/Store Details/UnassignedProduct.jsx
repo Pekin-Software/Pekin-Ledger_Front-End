6fa115ab353d9ef074ca92014e9d349f104b8dde
eskainah <eskainah@gmail.com>
2025-07-16 13:32:37 +0000

    hot fix

--- src/components/Admin Dashboard/Store Dashboard/Store Details/UnassignedProduct.jsx
+++ src/components/Admin Dashboard/Store Dashboard/Store Details/UnassignedProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { X } from "lucide-react";
 import ProductSection from "../../../Inventory/ProductDetails/products";
 import "./unassignedProduct.css";
